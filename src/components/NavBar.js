b1b55b50c3c447ec0a28e1b4c6dbd39e01036bcc
marcoshannecke <mhannecke@align-alytics.com>
2019-11-08 23:27:02 +0000

    fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,19 +7,18 @@ import About from "./About";
 import '../assets/scss/index.scss'
 import logo from "../assets/images/logo@2x.png";
 
-
 class NavBar extends Component {
     render() {
         return (
             <div className='Bridge'>
                 <Router>
-                    <Navbar expand="lg">
-                        <Navbar.Brand><img src={logo} width='154px' alt="logo"/></Navbar.Brand>
+                    <Navbar expand="lg" fixed="top">
+                        <Navbar.Brand><img src={logo} width='158px' className="pl-1" alt="logo"/></Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                         <Navbar.Collapse className="justify-content-end">
                             <Nav>
                                 <LinkContainer to="/main">
-                                    <Nav.Link>MAIN</Nav.Link>
+                                    <Nav.Link>HOME</Nav.Link>
                                 </LinkContainer>
                                 <LinkContainer to="/about">
                                     <Nav.Link>PAGES</Nav.Link>
@@ -34,7 +33,7 @@ class NavBar extends Component {
                                     <Nav.Link>FEATURES</Nav.Link>
                                 </LinkContainer>
                                 <LinkContainer to="/a">
-                                    <Nav.Link>COMPONENTS</Nav.Link>
+                                    <Nav.Link className="pr-2">COMPONENTS</Nav.Link>
                                 </LinkContainer>
                             </Nav>
                         </Navbar.Collapse>
@@ -46,7 +45,6 @@ class NavBar extends Component {
                             <Route path="/about" component={About}/>
                         </Switch>
                     </Container>
-
                 </Router>
             </div>
         );
